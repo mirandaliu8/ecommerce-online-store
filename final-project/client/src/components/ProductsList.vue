5bf892d8d488bccbfb363de1a36a6be364b93f0b
Miranda Liu <miranda.liu8@gmail.com>
2023-11-09 14:07:51 -0600

    removed redundant products header

--- final-project/client/src/components/ProductsList.vue
+++ final-project/client/src/components/ProductsList.vue
@@ -1,6 +1,5 @@
 <template>
   <section>
-    <h2>Products</h2>
   <div class="products-list">
     <product-card v-for="product in products" v-bind:product="product" v-bind:key="product.productId" v-bind:enable-add="true" />
   </div>
